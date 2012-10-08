2f6e5d110c8e69ebc8363be1418eb3c7e167668a
Casey Kuball <darthfett@gmail.com>
2012-10-08 15:46:40 -0700

    Adding empty main function

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from __future__ import print_function
+from __future__ import division
+
+# 3rd party modules
+import Image as pil
+
+# builtin modules
+import os
+import sys
+
+# project modules
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv[1:]) # Skip first argument ("main.py").
