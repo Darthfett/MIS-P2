09e1f52baa9b013b57a1d86c9f1de45efc0a1385
Casey Kuball <darthfett@gmail.com>
2012-10-17 17:12:24 -0700

    Working on adding basic functionality

--- main.py
+++ main.py
@@ -16,6 +16,35 @@ import predictive_encoding as pe
 import error_quantization as eq
 import encoding
 
+HELP = """
+
+display: display the image
+encoding: choose the encoding scheme
+error: choose the error quantization parameters
+help: display this message
+predict: choose a predictive encoding algorithm
+quantization: choose the number of bins to quantize
+reduce: choose the resolution reduction parameters
+save: save the image to the disk
+select: select a new image
+
+""".strip()
+
+def display(image, *args):
+    image.show()
+
+def help(image, *args):
+    print(HELP)
+
+def save(image, *args):
+    filename = raw_input('Filename: ')
+    try:
+        image.save(filename, 'PNG')
+    except Exception:
+        return None
+    return image
+
+
 
 def main(args):
     pass
