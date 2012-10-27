3317df26b463fa90f8bfadf4aa4b312260b6507f
Casey Kuball <darthfett@gmail.com>
2012-10-27 12:51:42 -0700

    Added get_image and get_channels

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import encoding
 
 HELP = """
 
-display: display the image
 encoding: choose the encoding scheme
 error: choose the error quantization parameters
 help: display this message
@@ -30,11 +29,52 @@ select: select a new image
 
 """.strip()
 
+Image = None
+
+def get_channels(image):
+    """Get a list of tuples containing the reds, greens, and blues of the image."""
+    RGB = image.getdata()
+    r, g, b = zip(*RGB) # matrix transform
+    return r, g, b
+
+def get_image():
+    image = None
+    while image is None:
+        image_path = raw_input("Select an image: ")
+        try:
+            image = pil.open(image_path)
+        except Exception:
+            pass
+    return image
+
+# Command functions
+
 def display(image, *args):
+    """
+    Attempts to display the image
+
+    NOTE: PIL's image.show can have problems on windows, as some viewers do not work properly.
+    See: http://stackoverflow.com/questions/8932976/python-imaging-library-show-on-windows
+    """
     image.show()
+
+def encoding_delegate(image, *args):
+    pass
+
+def error_delegate(image, *args):
+    pass
 
 def help(image, *args):
     print(HELP)
+
+def predict_delegate(image, *args):
+    pass
+
+def quantization_delegate(image, *args):
+    pass
+
+def reduce_delegate(image, *args):
+    pass
 
 def save(image, *args):
     filename = raw_input('Filename: ')
@@ -44,10 +84,25 @@ def save(image, *args):
         return None
     return image
 
+def select(image, *args):
+    Image = get_image()
 
+CMD_DICT = {
+    'display': display,
+    'encoding': encoding_delegate,
+    'error': error_delegate,
+    'help': help,
+    'predict': predict_delegate,
+    'quantization': quantization_delegate,
+    'reduce': reduce_delegate,
+    'save': save,
+    'select': select,
+}
 
 def main(args):
-    pass
+    Image = get_image()
+    //display(Image)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:]) # Skip first argument ("main.py").
